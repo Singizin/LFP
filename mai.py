250260d4713fbd5fcd7b9f135ce3da887c6767e6
singizin <igsing96@gmail.com>
2022-12-22 02:18:21 +0700

    2

--- mai.py
+++ mai.py
@@ -4,24 +4,26 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 df = pd.read_csv('files/3-65-3N.csv', sep=';')
-
+row = df.iloc[1]
+print(row['date'] + ' ' + row['time'])
+print(datetime.strptime(row['date'] + ' ' + row['time'], '%d.%m.%Y %H:%M:%S'))
 list_of_df = []
 new_df = pd.DataFrame(columns=df.columns)
 
 current_cycle = 0
 
 new_time = 0
-for index in range(0, df.shape[0], 10):
+for index in range(0, df.shape[0], 100):
     row = df.iloc[index]
     if row['cycle_1'] == 0:
         continue
     if row['cycle_1'] != current_cycle:
-        new_time = datetime.strptime(row['time'], '%H:%M:%S')
+        new_time = datetime.strptime(row['date'] + ' ' + row['time'], '%d.%m.%Y %H:%M:%S')
         current_cycle = row['cycle_1']
         list_of_df.append(new_df)
         new_df = pd.DataFrame(columns=df.columns)
     if new_time:
-        row['time'] = (datetime.strptime(row['time'], '%H:%M:%S') - new_time).seconds
+        row['time'] = (datetime.strptime(row['date'] + ' ' + row['time'], '%d.%m.%Y %H:%M:%S') - new_time).seconds
 
     new_df = new_df.append(row, ignore_index=True)
 
--- plottttt.py
+++ plottttt.py
@@ -3,15 +3,17 @@ import pandas as pd
 
 df = pd.read_csv('files/xxx.csv', sep=';')
 new_df = pd.DataFrame(columns=df.columns)
-print(df.iloc[[1], [2, 5]])
 list_of_df = []
 current_cycle = 1
 
+len_of_cycle = []
 
-for i in range(20):
-    selected = df.where(df['cycle_1'] == i)
+for i in range(90):
+    selected: pd.DataFrame = df.loc[df['cycle_1'] == i]
     list_of_df.append(selected)
 
-for i in list_of_df:
-    plt.plot(i.iloc[:, [2]], i.iloc[:, [5]])
+for i in list_of_df[10:30]:
+    plt.plot(i.iloc[:, [2]], i.iloc[:, [5]],linewidth=1)
+
+# print(len_of_cycle)
 plt.show()
